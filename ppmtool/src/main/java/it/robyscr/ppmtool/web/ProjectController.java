67cf40f7281a929da3148a338822c363e7191984
Unknown <roberto.scaramozza@gmail.com>
2020-05-05 00:17:52 +0200

    Section 2.12

--- ppmtool/src/main/java/it/robyscr/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/it/robyscr/ppmtool/web/ProjectController.java
@@ -1,9 +1,12 @@
 package it.robyscr.ppmtool.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +22,14 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
-		projectService.saveOrUpdateProject(project);
-		return new ResponseEntity<>(project,HttpStatus.CREATED);
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+		}
+		
+		Project projectSaved = projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<>(projectSaved,HttpStatus.CREATED);
 	}
 	
 }
