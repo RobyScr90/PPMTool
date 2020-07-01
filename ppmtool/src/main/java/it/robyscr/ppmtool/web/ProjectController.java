e962f4d6b346981b5b6cde7242435a905d219085
Unknown <roberto.scaramozza@gmail.com>
2020-07-02 00:04:41 +0200

    Section 2.17
    Find All Projects

--- ppmtool/src/main/java/it/robyscr/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/it/robyscr/ppmtool/web/ProjectController.java
@@ -5,19 +5,19 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.robyscr.ppmtool.domain.Project;
 import it.robyscr.ppmtool.services.MapValidationErrorService;
 import it.robyscr.ppmtool.services.ProjectService;
 
-@Controller
+@RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
@@ -42,4 +42,10 @@ public class ProjectController {
 		Project project = projectService.findProjectByIdenfier(projectId);
 		return new ResponseEntity<Project>(project,HttpStatus.OK);
 	}
+	
+	@GetMapping("/all")
+	public Iterable<Project> getAllProjects(){
+		return projectService.findAllProjects();
+	}
+	
 }
